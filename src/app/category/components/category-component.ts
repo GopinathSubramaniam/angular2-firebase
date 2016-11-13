49eae3d26fa5c29b7a1a9cd1482567a04f695d99
gopiwrld@gmail.com <gopiwrld@gmail.com>
2016-11-13 13:18:23 +0530

    Upload category icon image

--- src/app/category/components/category-component.ts
+++ src/app/category/components/category-component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 
+import {AppService} from '../../common/services/app-service';
 import {CategoryService} from '../services/category-service';
 
 @Component({
@@ -12,19 +13,41 @@ export class CategoryComponent{
     
     private categories = <any>[];
     private categoryName = '';
+    private file:any;
 
-    constructor(private categoryService: CategoryService){
+    constructor(
+        private categoryService: CategoryService,
+        private appService: AppService
+        ){
         this.categories = this.categoryService.getCategories();
     }
 
     addNewCategory(){
-        var catObj = {name: this.categoryName};
-        if(catObj.name){
-            this.categories.push(catObj);
-            this.categoryName = '';
+        var that = this;
+        if(this.file){
+            this.appService.uploadImage(this.file).then(function(imageUrl){
+                var catObj = {name: that.categoryName, iconUrl: imageUrl};
+                if(catObj.name){
+                    that.categories.push(catObj);
+                    that.file = '';
+                    that.categoryName = '';
+                }else{
+                    alert('Please Enter Category Name');
+                }
+            });
         }else{
-            alert('Please Enter Category Name');
+            alert('Please select category image');
         }
+        
+    }
+
+    getImage(ev:any){
+        console.log('Image :: ', ev.srcElement.files[0]);
+        this.file =  ev.srcElement.files[0]
+    }
+
+    deleteCategory(){
+        
     }
 
 }
